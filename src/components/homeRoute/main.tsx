48758ac1e404d12b9e966eb0c05c329ac6605407
sirWerq <rifqx1904@gmail.com>
2024-08-01 09:11:09 +0700

    "adding new feature -not 100%"

--- src/components/homeRoute/main.tsx
+++ src/components/homeRoute/main.tsx
@@ -3,6 +3,21 @@ import Card from "../card";
 import Timeline from "../timeline";
 
 const MainComponent = () => {
+  const handleSubmit: React.FormEventHandler<HTMLFormElement> = (e) => {
+    e.preventDefault();
+    const firstName = (document.getElementById("firstName") as HTMLInputElement)
+      .value;
+    const message = (document.getElementById("message") as HTMLTextAreaElement)
+      .value;
+
+    const urlWa = `https://wa.me/6281220884573?text=${encodeURIComponent(
+      `Halo, my name is ${firstName}, and ${message}`
+    )}`;
+    setTimeout(() => {
+      window.open(urlWa, "_blank");
+    }, 100); // Menambahkan delay 100ms
+  };
+
   return (
     <main className="px-4 w-full">
       <div className="flex w-full justify-center pt-[80px] h-screen">
@@ -38,7 +53,7 @@ const MainComponent = () => {
             </div>
             <div className="hidden lg:block mr-10">
               <img
-                src="../src/assets/anime.jpeg"
+                src="../src/assets/rifqi.jpg"
                 alt=""
                 className="rounded-lg w-[400px] h-[400px]"
               />
@@ -185,14 +200,13 @@ const MainComponent = () => {
               </p>
             </div>
             <form
-              action="#"
-              method="POST"
               className="mx-auto mt-16 max-w-xl sm:mt-20"
+              onSubmit={(e) => handleSubmit(e)}
             >
-              <div className="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
+              <div className="grid grid-cols-1">
                 <div>
                   <label
-                    htmlFor="first-name"
+                    htmlFor="firstName"
                     className="block text-sm font-semibold leading-6 text-gray-900"
                   >
                     First name
@@ -200,60 +214,9 @@ const MainComponent = () => {
                   <div className="mt-2.5">
                     <input
                       type="text"
-                      name="first-name"
-                      id="first-name"
+                      name="firstName"
+                      id="firstName"
                       autoComplete="given-name"
-                      className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                    />
-                  </div>
-                </div>
-                <div>
-                  <label
-                    htmlFor="last-name"
-                    className="block text-sm font-semibold leading-6 text-gray-900"
-                  >
-                    Last name
-                  </label>
-                  <div className="mt-2.5">
-                    <input
-                      type="text"
-                      name="last-name"
-                      id="last-name"
-                      autoComplete="family-name"
-                      className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                    />
-                  </div>
-                </div>
-                <div className="sm:col-span-2">
-                  <label
-                    htmlFor="email"
-                    className="block text-sm font-semibold leading-6 text-gray-900"
-                  >
-                    Email
-                  </label>
-                  <div className="mt-2.5">
-                    <input
-                      type="email"
-                      name="email"
-                      id="email"
-                      autoComplete="email"
-                      className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                    />
-                  </div>
-                </div>
-                <div className="sm:col-span-2">
-                  <label
-                    htmlFor="phone-number"
-                    className="block text-sm font-semibold leading-6 text-gray-900"
-                  >
-                    Phone number
-                  </label>
-                  <div className="relative mt-2.5">
-                    <input
-                      type="tel"
-                      name="phone-number"
-                      id="phone-number"
-                      autoComplete="tel"
                       className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                     />
                   </div>
@@ -277,7 +240,7 @@ const MainComponent = () => {
               <div className="mt-10">
                 <button
                   type="submit"
-                  className="block w-full rounded-md bg-sky-500 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                  className="block w-full rounded-md bg-sky-500 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-sky-700 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                 >
                   Let's talk
                 </button>
